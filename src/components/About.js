a6ed0d09d4eb05388d7493a23b7b092ba22b0e7e
Aditi Jain <115243528+ADITI21-pi@users.noreply.github.com>
2026-06-24 17:34:57 +0000

    Update portfolio content

--- src/components/About.js
+++ src/components/About.js
@@ -1,45 +1,112 @@
 import React from "react";
 import Aboutdisc from "./Aboutdisc";
 import Circlebar from "./Circlebar";
-import { CircularProgressbar, CircularProgressbarWithChildren } from "react-circular-progressbar";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import Upnav from "./Upnav";
+
 const About = () => {
   return (
-    <div className=" min-h-screen text-white flex justify-center items-center p-7 vsm:p-0 vsm:pt-3 flex-col">
+    <div className="min-h-screen text-white flex justify-center items-center flex-col p-7 vsm:p-0 vsm:pt-3">
+      
+      {/* About Heading */}
       <div className="heading">
-        <h1 className="cursive mt-5  text-7xl vsm:text-5xl">About me</h1>
+        <h1 className="cursive mt-5 text-7xl vsm:text-5xl">
+          About me
+        </h1>
+
         <div className="underlineanim border-cyan-500 border w-64 duration-500 mt-3 vsm:w-60"></div>
       </div>
-      <div className=" m-20 gap-32 flex flex-row xl:flex-col lg:flex-row md:flex-col justify-between items-center">
+
+      {/* Main Content */}
+      <div
+        className="
+          w-full
+          max-w-7xl
+          mx-auto
+          px-6
+          py-10
+          flex
+          flex-row
+          xl:flex-col
+          lg:flex-row
+          md:flex-col
+          gap-20
+          justify-between
+          items-center
+        "
+      >
+        {/* About Description */}
         <div className="flex flex-row justify-between items-center">
-          <Aboutdisc/>
+          <Aboutdisc />
         </div>
-        <div className="">
-          <div className="heading">
-            <h1 className=" cursive text-3xl ">My Skills</h1>
-            <div className="underlineanim border-cyan-500 border w-40 duration-500 mt-3"></div>
+
+        {/* Skills Section */}
+        <div
+          className="
+            w-full
+            max-w-3xl
+            bg-white/5
+            backdrop-blur-md
+            border
+            border-cyan-500/20
+            rounded-3xl
+            p-8
+            shadow-[0_0_25px_rgba(34,211,238,0.1)]
+            hover:border-cyan-400/40
+            transition-all
+            duration-500
+          "
+        >
+          {/* Skills Heading */}
+          <div className="mb-8">
+            <span className="text-cyan-400 uppercase tracking-[4px] text-sm">
+              Expertise
+            </span>
+
+            <h1
+              className="
+                text-4xl
+                font-bold
+                text-white
+                mt-1
+                tracking-wide
+                drop-shadow-[0_0_10px_rgba(34,211,238,0.5)]
+              "
+              style={{ fontFamily: "Poppins, sans-serif" }}
+            >
+              My Skills
+            </h1>
+
+            <div className="w-20 h-1 bg-cyan-400 rounded-full mt-3"></div>
           </div>
-          <div className=" mt-10 grid grid-cols-5 gap-x-20 gap-y-10 xl:grid-cols-4 md:grid-cols-4 vsm:grid-cols-3">
-            {/* <div className="flex flex-row justify-center items-center mt-2"> */}
-                <Circlebar val='60' topic='Java'/>
-                <Circlebar val='45' topic='DSA'/>
-                <Circlebar val='25' topic='Python'/>
-            {/* </div> */}
-            {/* <div className="flex flex-row justify-center items-center mt-2"> */}
-                <Circlebar val='80' topic='HTML'/>
-                <Circlebar val='80' topic='CSS'/>
-                <Circlebar val='40' topic='JavaScript'/>
-            {/* </div> */}
-            {/* <div className="flex flex-row justify-center items-center mt-2"> */}
-                <Circlebar val='60' topic='React.js'/>
-                <Circlebar val='60' topic='Node.js'/>
-                <Circlebar val='60' topic='SQL'/>
-            {/* </div> */}
-            {/* <div className="flex flex-row justify-center items-center mt-2"> */}
-                <Circlebar val='80' topic='Bootstrap'/>
-                <Circlebar val='60' topic='Tailwind CSS'/>
-                <Circlebar val='80' topic='Git & GitHub'/>
-            {/* </div> */}
+
+          {/* Skills Grid */}
+          <div
+            className="
+              mt-8
+              grid
+              grid-cols-5
+              xl:grid-cols-4
+              lg:grid-cols-4
+              md:grid-cols-3
+              sm:grid-cols-3
+              vsm:grid-cols-2
+              gap-x-10
+              gap-y-10
+            "
+          >
+            <Circlebar val="20" topic="Java" />
+            <Circlebar val="30" topic="DSA" />
+            <Circlebar val="80" topic="HTML" />
+            <Circlebar val="80" topic="CSS" />
+            <Circlebar val="60" topic="JavaScript" />
+
+            <Circlebar val="50" topic="React.js" />
+            <Circlebar val="40" topic="Redux" />
+
+            <Circlebar val="70" topic="Bootstrap" />
+            <Circlebar val="60" topic="Tailwind CSS" />
+            <Circlebar val="80" topic="Git & GitHub" />
           </div>
         </div>
       </div>
--- src/components/Circlebar.js
+++ src/components/Circlebar.js
@@ -1,12 +1,58 @@
-import React from 'react'
-import { CircularProgressbar, CircularProgressbarWithChildren } from "react-circular-progressbar";
+import React from "react";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
 const Circlebar = (props) => {
   return (
-    <div className="flex flex-col justify-center items-center ">
-        <CircularProgressbar value={props.val} className="bar w-14 xl:w-20 lg:w-14 md:w-20 vsm:w-14"/>
-        <h1>{props.topic}</h1>
+    <div
+      className="
+        flex
+        flex-col
+        items-center
+        transition-all
+        duration-300
+        hover:scale-110
+        hover:-translate-y-1
+        cursor-pointer
+      "
+    >
+      <div
+        className="
+          p-2
+          rounded-full
+          hover:shadow-[0_0_20px_rgba(34,211,238,0.4)]
+          transition-all
+          duration-300
+        "
+      >
+        <CircularProgressbar
+          value={props.val}
+          text={`${props.val}%`}
+          className="w-16 xl:w-20 lg:w-16 md:w-20 vsm:w-16"
+          styles={buildStyles({
+            textSize: "20px",
+            textColor: "#ffffff",
+            pathColor: "#22d3ee",
+            trailColor: "#2d2d2d",
+            pathTransitionDuration: 1.5,
+          })}
+        />
+      </div>
+
+      <p
+        className="
+          mt-3
+          text-sm
+          text-gray-200
+          font-medium
+          text-center
+        "
+        style={{ fontFamily: "Poppins, sans-serif" }}
+      >
+        {props.topic}
+      </p>
     </div>
-  )
-}
+  );
+};
 
 export default Circlebar;
